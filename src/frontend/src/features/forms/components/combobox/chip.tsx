suitenumerique/messages

✨(frontend) compose messages in floating windows
Composing moved from the full-page /new route to floating windows
docked at the bottom of the mailbox: several drafts can be open side by
side, minimized, expanded to full-screen, and they survive a reload
through persisted window descriptors kept in sync across tabs. On
mobile the windows collapse into a stack bar with an overview to switch
between them. A draft being edited in a window is replaced inline by a
placeholder pointing at it, so it can never be edited twice. The old
/new route and thread drafts now open a window, and the e2e suite is
adapted to compose through it.

src/frontend/src/features/forms/components/combobox/chip.tsx
@@ -1,18 +1,30 @@
-import { Icon } from "@gouvfr-lasuite/ui-kit";
-import { Button } from "@gouvfr-lasuite/cunningham-react";
+import { Icon, IconType, Tooltip } from "@gouvfr-lasuite/ui-components";
+import { Button } from "@gouvfr-lasuite/ui-components";
+import clsx from "clsx";
 import { HTMLAttributes } from "react";
 import { useTranslation } from "react-i18next";
 
 type ChipProps = HTMLAttributes<HTMLDivElement> & {
     label: string;
     onRemove: () => void;
+    // Short warning attached to this value. Rendered as an icon in the chip
+    // and read out to assistive technologies; the tooltip shown while the
+    // chip is hovered or focused is a desktop bonus only, the surrounding
+    // field is expected to carry the text where hover does not exist (touch
+    // devices).
+    warning?: string;
 }
 
-export const Chip = ({label, onRemove, ...props}: ChipProps) => {
+export const Chip = ({ label, onRemove, warning, className, ...props }: ChipProps) => {
     const { t } = useTranslation();
 
-    return (
-        <div className="c__combobox__chip" {...props}>
+    const chip = (
+        <div className={clsx("c__combobox__chip", { "c__combobox__chip--warning": warning }, className)} {...props}>
+            {warning && (
+                <span className="c__combobox__chip__warning" role="img" aria-label={warning}>
+                    <Icon name="warning" type={IconType.OUTLINED} />
+                </span>
+            )}
             <span className="c__combobox__chip__label">{label}</span>
             {
                 onRemove && (
@@ -31,5 +43,15 @@ export const Chip = ({label, onRemove, ...props}: ChipProps) => {
                 )
             }
         </div>
+    );
+
+    if (!warning) return chip;
+    // The tooltip trigger takes over the `ref` of its child, and the chip's
+    // ref belongs to downshift (keyboard navigation between chips): anchor
+    // the tooltip on a wrapper so both keep working.
+    return (
+        <Tooltip content={warning} placement="top">
+            <span className="c__combobox__chip__tooltip-anchor">{chip}</span>
+        </Tooltip>
     );
 }

bmann/lasuite-messages

src/frontend/src/features/forms/components/combobox/chip.tsx
@@ -1,18 +1,30 @@
-import { Button } from "@gouvfr-lasuite/cunningham-react";
+import { Button, IconType, Tooltip } from "@gouvfr-lasuite/cunningham-react";
 import { Icon } from "@/features/ui/components/icon";
+import clsx from "clsx";
 import { HTMLAttributes } from "react";
 import { useTranslation } from "react-i18next";
 
 type ChipProps = HTMLAttributes<HTMLDivElement> & {
     label: string;
     onRemove: () => void;
+    // Short warning attached to this value. Rendered as an icon in the chip
+    // and read out to assistive technologies; the tooltip shown while the
+    // chip is hovered or focused is a desktop bonus only, the surrounding
+    // field is expected to carry the text where hover does not exist (touch
+    // devices).
+    warning?: string;
 }
 
-export const Chip = ({label, onRemove, ...props}: ChipProps) => {
+export const Chip = ({ label, onRemove, warning, className, ...props }: ChipProps) => {
     const { t } = useTranslation();
 
-    return (
-        <div className="c__combobox__chip" {...props}>
+    const chip = (
+        <div className={clsx("c__combobox__chip", { "c__combobox__chip--warning": warning }, className)} {...props}>
+            {warning && (
+                <span className="c__combobox__chip__warning" role="img" aria-label={warning}>
+                    <Icon name="warning" type={IconType.OUTLINED} />
+                </span>
+            )}
             <span className="c__combobox__chip__label">{label}</span>
             {
                 onRemove && (
@@ -31,5 +43,15 @@ export const Chip = ({label, onRemove, ...props}: ChipProps) => {
                 )
             }
         </div>
+    );
+
+    if (!warning) return chip;
+    // The tooltip trigger takes over the `ref` of its child, and the chip's
+    // ref belongs to downshift (keyboard navigation between chips): anchor
+    // the tooltip on a wrapper so both keep working.
+    return (
+        <Tooltip content={warning} placement="top">
+            <span className="c__combobox__chip__tooltip-anchor">{chip}</span>
+        </Tooltip>
     );
 }